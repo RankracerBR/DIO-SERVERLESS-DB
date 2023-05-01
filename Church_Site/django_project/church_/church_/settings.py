55c36f582b123735863d37855b4d4ea0f843aa4f
RankracerBR <101043200+RankracerBR@users.noreply.github.com>
2023-05-01 16:03:04 -0300

    project

--- Church_Site/django_project/church_/church_/settings.py
+++ Church_Site/django_project/church_/church_/settings.py
@@ -129,8 +129,8 @@ STATICFILES_DIRS = [
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'rankracerbr21@gmail.com'
+EMAIL_HOST_PASSWORD = 'odgmeewzybimozyy'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 # Default primary key field type
